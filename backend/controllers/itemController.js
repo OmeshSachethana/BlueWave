0d96debec06e9d3beace987c31be6a713ff3577d
Omesh Sachethana <sachethana12@gmail.com>
2024-09-10 22:00:43 +0530

    createItem

---
+++ backend/controllers/itemController.js
@@ -0,0 +1,19 @@
+const Item = require('../models/item');
+
+// Create a new item
+exports.createItem = async (req, res) => {
+    try {
+        const newItem = new Item(req.body);
+        await newItem.save();
+        res.status(201).json(newItem);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+
+
+
+
+
+
