dd484b81ab5056f31ff953e6a56597dfd66fbbfb
Omesh Sachethana <sachethana12@gmail.com>
2024-09-10 22:00:55 +0530

    Read item

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -11,7 +11,15 @@ exports.createItem = async (req, res) => {
     }
 };
 
-
+// Read all items
+exports.getItems = async (req, res) => {
+    try {
+        const items = await Item.find();
+        res.status(200).json(items);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 
 
