24e1bcd9f0583f545753d7efb55a14c7cf78a80f
Omesh Sachethana <sachethana12@gmail.com>
2024-09-10 23:17:17 +0530

    created Employee Schema

---
+++ backend/models/employee.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+// Define the Employee schema
+const employeeSchema = new mongoose.Schema({
+  employeeID: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  position: {
+    type: String,
+    required: true
+  },
+  department: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female', 'Other'],
+    required: true
+  },
+  nic: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+  }
+}, { timestamps: true });
+
+// Create the Employee model
+const Employee = mongoose.model('Employee', employeeSchema);
+
+module.exports = Employee;
