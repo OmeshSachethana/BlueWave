b2997f18a40a74b165e0a521777d036358d139c3
Omesh Sachethana <sachethana12@gmail.com>
2024-09-11 10:58:00 +0530

    fixed

--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const scheduleController = require('../controllers/scheduleController');
 
 router.post('/', scheduleController.createSchedule);
-router.get('/', scheduleController.get);
+router.get('/', scheduleController.getAllSchedules);
 // router.get('/:id', scheduleController.);
 // router.put('/:id', scheduleController.);
 // router.delete('/:id', scheduleController.);
