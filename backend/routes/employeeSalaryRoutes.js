3dda15e5b707b0cb271190d6a43a172a2738d484
Omesh Sachethana <sachethana12@gmail.com>
2024-09-11 09:13:21 +0530

    changed ids

--- backend/routes/employeeSalaryRoutes.js
+++ backend/routes/employeeSalaryRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const employeeSalaryController = require('../controllers/employeeSalaryController');
 
 router.post('/', employeeSalaryController.createEmployeeSalary);
-router.get('/:id', employeeSalaryController.getEmployeeSalary);
-router.put('/:id', employeeSalaryController.updateEmployeeSalary);
-router.delete('/:id', employeeSalaryController.deleteEmployeeSalary);
+router.get('/:employeeID', employeeSalaryController.getEmployeeSalary);
+router.put('/:employeeID', employeeSalaryController.updateEmployeeSalary);
+router.delete('/:employeeID', employeeSalaryController.deleteEmployeeSalary);
 
 module.exports = router;
