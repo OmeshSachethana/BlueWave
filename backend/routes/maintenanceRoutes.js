4cffe1649d2da5fc822611aeca28ffb072e4a93f
Omesh Sachethana <sachethana12@gmail.com>
2024-09-11 12:38:13 +0530

    updated

--- backend/routes/maintenanceRoutes.js
+++ backend/routes/maintenanceRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const maintenanceController = require('../controllers/maintenanceController');
 
-router.post('/', maintenanceController.createEmployee);
-router.get('/', maintenanceController.getAllEmployees);
+router.post('/', maintenanceController.createMaintenance);
+router.get('/', maintenanceController.getAllMaintenanceRecords);
 // router.get('/:id', maintenanceController.getEmployeeById);
 // router.put('/:id', maintenanceController.updateEmployee);
 // router.delete('/:id', maintenanceController.deleteEmployee);
