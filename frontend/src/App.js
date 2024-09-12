99d84d8c93eb1509fac7963a6181397966caf814
Omesh Sachethana <sachethana12@gmail.com>
2024-09-12 23:39:10 +0530

    connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import EmployeeList from './components/employee/EmployeeList';
+import EmployeeForm from './components/employee/EmployeeForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          BlueWay <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <EmployeeForm />
+        <EmployeeList />
+      </div>
+    </Provider>
   );
 }
 
