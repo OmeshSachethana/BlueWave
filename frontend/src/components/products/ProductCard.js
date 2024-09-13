cf46802731a5109a175b4f095a474052bac906ff
Menusha Katipearachchi <ekettipearachchi@gmail.com>
2024-09-13 11:49:29 +0530

    Refactor Navbar component to include cart functionality

--- frontend/src/components/products/ProductCard.js
+++ frontend/src/components/products/ProductCard.js
@@ -9,16 +9,23 @@ const ProductCard = ({ product }) => {
   };
 
   return (
-    <div className="border p-4 rounded shadow-lg">
-      <h2 className="text-lg font-bold mb-2">{product.name}</h2>
-      <p className="text-xl mb-2">Rs:{product.price.toFixed(2)}</p>
+    <a
+      href="#"
+      className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    >
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {product.name}
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        Rs: {product.price.toFixed(2)}
+      </p>
       <button
-        className="bg-blue-500 text-white px-4 py-2 rounded"
+        className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
         onClick={handleAddToCart}
       >
         Add to Cart
       </button>
-    </div>
+    </a>
   );
 };
 
