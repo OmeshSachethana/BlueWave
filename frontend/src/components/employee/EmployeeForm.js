3f85fc121396876899882a013995ad65ac7f4ecc
Omesh Sachethana <sachethana12@gmail.com>
2024-09-13 09:19:15 +0530

    br

--- frontend/src/components/employee/EmployeeForm.js
+++ frontend/src/components/employee/EmployeeForm.js
@@ -37,13 +37,14 @@ const EmployeeForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="employeeID" value={employee.employeeID} onChange={handleChange} placeholder="Employee ID" />
-      <input type="text" name="firstName" value={employee.firstName} onChange={handleChange} placeholder="First Name" />
-      <input type="text" name="lastName" value={employee.lastName} onChange={handleChange} placeholder="Last Name" />
-      <input type="text" name="position" value={employee.position} onChange={handleChange} placeholder="Position" />
-      <input type="text" name="department" value={employee.department} onChange={handleChange} placeholder="Department" />
-      <input type="text" name="gender" value={employee.gender} onChange={handleChange} placeholder="Gender" />
-      <input type="text" name="nic" value={employee.nic} onChange={handleChange} placeholder="NIC" />
+      <h1>Add Employee</h1>
+      <input type="text" name="employeeID" value={employee.employeeID} onChange={handleChange} placeholder="Employee ID" /><br/>
+      <input type="text" name="firstName" value={employee.firstName} onChange={handleChange} placeholder="First Name" /><br/>
+      <input type="text" name="lastName" value={employee.lastName} onChange={handleChange} placeholder="Last Name" /><br/>
+      <input type="text" name="position" value={employee.position} onChange={handleChange} placeholder="Position" /><br/>
+      <input type="text" name="department" value={employee.department} onChange={handleChange} placeholder="Department" /><br/>
+      <input type="text" name="gender" value={employee.gender} onChange={handleChange} placeholder="Gender" /><br/>
+      <input type="text" name="nic" value={employee.nic} onChange={handleChange} placeholder="NIC" /><br/>
       <input type="email" name="email" value={employee.email} onChange={handleChange} placeholder="Email" />
       <button type="submit">Add Employee</button>
     </form>
