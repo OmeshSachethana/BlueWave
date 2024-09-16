ff5ff876b52b1ef8bc38b9351b64b0429745c0d6
Omesh Sachethana <sachethana12@gmail.com>
2024-09-17 02:06:53 +0530

    payment create

--- frontend/src/components/orders/PaymentPage.js
+++ frontend/src/components/orders/PaymentPage.js
@@ -1,31 +1,50 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
-import { updatePaymentStatus } from "../../services/orderService";
+import { useDispatch, useSelector } from "react-redux";
+import { createPayment } from "../../features/payment/paymentSlice";
 
 const PaymentPage = () => {
   const location = useLocation();
   const { orderId, orderAmount } = location.state || {};
+  const dispatch = useDispatch();
 
-  const [formData, setFormData] = useState();
+  const [formData, setFormData] = useState({
+    full_name: "",
+    cardNumber: "",
+    expiryDate: "",
+    cvv: "",
+    type: "Credit Card", // or "Debit Card"
+  });
+
+  const { loading, error } = useSelector((state) => state.payment);
 
   const handleChange = (e) => {
-    setFormData({ [e.target.id]: e.target.value });
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Handle payment submission logic here
-    console.log("Form data submitted:", formData);
+    const paymentData = {
+      type: formData.type,
+      cardNumber: formData.cardNumber,
+      name: formData.full_name,
+      expiryDate: formData.expiryDate,
+      cvv: formData.cvv,
+    };
 
-    try {
-      // Assuming payment goes through successfully, update the payment status to "Completed"
-      await updatePaymentStatus(orderId, "Completed");
-      alert(`Payment of Rs.${orderAmount} for Order #${orderId} completed!`);
-    } catch (error) {
-      console.error("Payment status update failed:", error);
-      alert("Error processing payment. Please try again.");
-    }
+    // Dispatch createPayment action
+    dispatch(createPayment(paymentData))
+      .unwrap()
+      .then(() => {
+        alert(`Payment of Rs.${orderAmount} for Order #${orderId} completed!`);
+      })
+      .catch((error) => {
+        alert("Error processing payment. Please try again.");
+      });
   };
 
   return (
@@ -52,6 +71,7 @@ const PaymentPage = () => {
                   <input
                     type="text"
                     id="full_name"
+                    value={formData.full_name}
                     onChange={handleChange}
                     className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
                     placeholder="Bonnie Green"
@@ -61,16 +81,17 @@ const PaymentPage = () => {
 
                 <div className="col-span-2 sm:col-span-1">
                   <label
-                    htmlFor="card-number-input"
+                    htmlFor="cardNumber"
                     className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Card number*
                   </label>
                   <input
                     type="text"
-                    id="card-number-input"
+                    id="cardNumber"
+                    value={formData.cardNumber}
                     onChange={handleChange}
-                    className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pe-10 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
+                    className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
                     placeholder="xxxx-xxxx-xxxx-xxxx"
                     required
                   />
@@ -78,76 +99,33 @@ const PaymentPage = () => {
 
                 <div>
                   <label
-                    htmlFor="card-expiration-input"
+                    htmlFor="expiryDate"
                     className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Card expiration*
                   </label>
-                  <div className="relative">
-                    <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3.5">
-                      <svg
-                        className="h-4 w-4 text-gray-500 dark:text-gray-400"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M5 5a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1 2 2 0 0 1 2 2v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a2 2 0 0 1 2-2ZM3 19v-7a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Zm6.01-6a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm-10 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </div>
-                    <input
-                      id="card-expiration-input"
-                      type="text"
-                      onChange={handleChange}
-                      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 ps-9 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                      placeholder="12/23"
-                      required
-                    />
-                  </div>
+                  <input
+                    id="expiryDate"
+                    type="text"
+                    value={formData.expiryDate}
+                    onChange={handleChange}
+                    className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                    placeholder="12/23"
+                    required
+                  />
                 </div>
+
                 <div>
                   <label
-                    htmlFor="cvv-input"
-                    className="mb-2 flex items-center gap-1 text-sm font-medium text-gray-900 dark:text-white"
+                    htmlFor="cvv"
+                    className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
                   >
                     CVV*
-                    <button
-                      data-tooltip-target="cvv-desc"
-                      data-tooltip-trigger="hover"
-                      className="text-gray-400 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
-                    >
-                      <svg
-                        className="h-4 w-4"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.408-5.5a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1h-2Z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </button>
-                    <div
-                      id="cvv-desc"
-                      role="tooltip"
-                      className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
-                    >
-                      The last 3 digits on back of card
-                      <div className="tooltip-arrow" data-popper-arrow></div>
-                    </div>
                   </label>
                   <input
-                    type="number"
-                    id="cvv-input"
+                    type="text"
+                    id="cvv"
+                    value={formData.cvv}
                     onChange={handleChange}
                     className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
                     placeholder="•••"
@@ -160,22 +138,26 @@ const PaymentPage = () => {
                 type="submit"
                 className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
-                Pay now
+                {loading ? "Processing..." : "Pay now"}
               </button>
+              {error && (
+                <p className="mt-4 text-sm text-red-500">
+                  {error.message || "An error occurred"}
+                </p>
+              )}
             </form>
 
             <div className="mt-6 grow sm:mt-8 lg:mt-0">
               <div className="space-y-4 rounded-lg border border-gray-100 bg-gray-50 p-6 dark:border-gray-700 dark:bg-gray-800">
-                <div className="space-y-2">
-                  <dl className="flex items-center justify-between gap-4">
-                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-                      Original price
-                    </dt>
-                    <dd className="text-base font-medium text-gray-900 dark:text-white">
-                      Rs.{orderAmount}
-                    </dd>
-                  </dl>
-                </div>
+                <dl className="flex items-center justify-between gap-4">
+                  <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                    Original price
+                  </dt>
+                  <dd className="text-base font-medium text-gray-900 dark:text-white">
+                    Rs.{orderAmount}
+                  </dd>
+                </dl>
+
                 <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
                   <dt className="text-base font-bold text-gray-900 dark:text-white">
                     Total
@@ -185,61 +167,8 @@ const PaymentPage = () => {
                   </dd>
                 </dl>
               </div>
-
-              <div className="mt-6 flex items-center justify-center gap-8">
-                <img
-                  className="h-8 w-auto dark:hidden"
-                  src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/brand-logos/paypal.svg"
-                  alt="PayPal"
-                />
-                <img
-                  className="hidden h-8 w-auto dark:flex"
-                  src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/brand-logos/paypal-dark.svg"
-                  alt="PayPal Dark"
-                />
-                <img
-                  className="h-8 w-auto dark:hidden"
-                  src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/brand-logos/visa.svg"
-                  alt="Visa"
-                />
-                <img
-                  className="hidden h-8 w-auto dark:flex"
-                  src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/brand-logos/visa-dark.svg"
-                  alt="Visa Dark"
-                />
-                <img
-                  className="h-8 w-auto dark:hidden"
-                  src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/brand-logos/mastercard.svg"
-                  alt="MasterCard"
-                />
-                <img
-                  className="hidden h-8 w-auto dark:flex"
-                  src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/brand-logos/mastercard-dark.svg"
-                  alt="MasterCard Dark"
-                />
-              </div>
             </div>
           </div>
-
-          <p className="mt-6 text-center text-gray-500 dark:text-gray-400 sm:mt-8 lg:text-left">
-            Payment processed by{" "}
-            <a
-              href="#"
-              title=""
-              className="font-medium text-primary-700 underline hover:no-underline dark:text-primary-500"
-            >
-              BlueWave
-            </a>{" "}
-            for{" "}
-            <a
-              href="#"
-              title=""
-              className="font-medium text-primary-700 underline hover:no-underline dark:text-primary-500"
-            >
-              BlueWave.lk
-            </a>
-            - Sri Lanka
-          </p>
         </div>
       </div>
     </section>
