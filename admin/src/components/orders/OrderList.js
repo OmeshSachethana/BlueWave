5ebd2c0cdea430c643fe5c5b421ae425895853fc
Menusha Katipearachchi <ekettipearachchi@gmail.com>
2024-09-17 23:03:52 +0530

    Fixed where the order details were not present in admin view

--- admin/src/components/orders/OrderList.js
+++ admin/src/components/orders/OrderList.js
@@ -258,6 +258,31 @@ const OrderList = () => {
                   )}
                 </div>
 
+                <div className="flex flex-col lg:flex-row lg:items-center justify-between px-6 pb-6 border-b border-gray-200">
+                  <div className="data">
+                    <p className="font-semibold text-base leading-7 text-black">
+                      Order Id:{" "}
+                      <span className="text-indigo-600 font-medium">
+                        #{order._id}
+                      </span>
+                    </p>
+                    <p className="font-semibold text-base leading-7 text-black mt-4">
+                      Order Date:{" "}
+                      <span className="text-gray-400 font-medium">
+                        {new Date(order.createdAt).toLocaleString("en-GB", {
+                          hour12: false,
+                        })}
+                      </span>
+                    </p>
+                    <p className="font-semibold text-base leading-7 text-black mt-2">
+                      Payment Method:{" "}
+                      <span className="text-gray-400 font-medium">
+                        {order.paymentMethod}
+                      </span>
+                    </p>
+                  </div>
+                </div>
+
                 {order.orderDetails.map((item, index) => (
                   <div
                     key={index}
