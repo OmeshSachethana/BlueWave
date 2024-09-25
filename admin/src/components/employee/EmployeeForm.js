798e3fb7ef493e0477bf4db0f8ae76dc3a50bd5b
Omesh Sachethana <sachethana12@gmail.com>
2024-09-25 19:41:42 +0530

    added department fields

--- admin/src/components/employee/EmployeeForm.js
+++ admin/src/components/employee/EmployeeForm.js
@@ -162,9 +162,11 @@ const EmployeeForm = ({ employeeToEdit }) => {
             required
           >
             <option value="">Select Department</option>
-            <option value="HR">HR</option>
-            <option value="Engineering">Engineering</option>
             <option value="Sales">Sales</option>
+            <option value="HR">HR</option>
+            <option value="Marketing">Marketing</option>
+            <option value="Production">Production</option>
+            <option value="Finance">Finance</option>
           </select>
         </div>
 
