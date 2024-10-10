fd439154230fcbdd2b5e121e2ac3723795400669
Omesh Sachethana <sachethana12@gmail.com>
2024-10-10 10:06:07 +0530

    button location changed

--- admin/src/components/pettyCash/PettyCashView.js
+++ admin/src/components/pettyCash/PettyCashView.js
@@ -142,7 +142,7 @@ const PettyCashView = () => {
         className="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
       >
         Back
-      </button>
+      </button> <br />
       <button 
         onClick={generatePDF}
         className="mb-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
